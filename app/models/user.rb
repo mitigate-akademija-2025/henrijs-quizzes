ab2edd953a8b7927e4d1039827f48fb1c531bed8
Henrijs Kravals <henrijskravals@gmail.com>
2025-08-09 11:40:33 +0300

    feat: establish associations for Question, Quiz, and User models

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable,
          :trackable, :lockable
   validates :username, presence: true, uniqueness: { case_sensitive: false }
+
+  has_many :quizzes, dependent: :destroy
 end
