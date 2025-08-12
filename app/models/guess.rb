fab9fbcdf943fc25077c59d8d96ec2e59e3ebfba
Henrijs Kravals <henrijskravals@gmail.com>
2025-08-12 21:45:37 +0300

    feat!: STI for different guess types

--- app/models/guess.rb
+++ app/models/guess.rb
@@ -1,10 +1,18 @@
-# app/models/guess.rb
 class Guess < ApplicationRecord
+  self.inheritance_column = :type
   belongs_to :game
-  belongs_to :option
-  has_one :question, through: :option
+  belongs_to :question
+  belongs_to :option, optional: true
+end
 
-  validates :game_id, :option_id, presence: true
-
+class ChoiceGuess < Guess
+  validates :option_id, presence: true
+  validates :answer_text, absence: true
   validates :option_id, uniqueness: { scope: :game_id }
+end
+
+class TextGuess < Guess
+  validates :option_id, absence: true
+  validates :answer_text, presence: true
+  validates :question_id, uniqueness: { scope: :game_id }
 end
